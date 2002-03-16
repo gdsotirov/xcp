7f20d6274b81cea3a96e92004ca8e2b56736b42d
Georgi D. Sotirov <gdsotirov@dir.bg>
2002-03-16 10:56:44 +0000

    Implement third command argument for buffer size

--- xcp.c
+++ xcp.c
@@ -1,13 +1,27 @@
+#include <stdio.h>
+
 #include "libxcpy.h"
 
 int main(int argc, char* argv[]) {
+    int bufsz = 0;
+
     if ( argc < 3 ) {
-        printf("Usage: %s <source> <destination>\n", argv[0]);
+        printf("Usage: %s <source> <destination> [<buffer size in bytes>]\n", argv[0]); 
         return 0;
     }
 
     FILENAME source = argv[1];
     FILENAME destination = argv[2];
+
+    /* Set buffer size if provided */
+    if ( argc == 4 ) {
+        bufsz = atoi(argv[3]);
+        if ( bufsz <= 1024 ) {
+            printf("error: Buffer size should be larger or equal to 1024 bytes.\n");
+            return 1;
+        }
+        set_bufsize(bufsz);
+    }    
 
     filecopy(source, destination);
 
